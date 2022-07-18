c40bd604fb9e29e433d18cde8ba834e2ddab0401
lpenafuentes <leonardo.pena@forcast.cl>
2022-07-18 16:52:52 -0400

    add localstorage and finish app

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchGifsResponse } from '../interfaces/gifs.interface';
 
 // root -- angular lo eleva a un nivel global
 @Injectable({
@@ -6,10 +8,24 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
 
+  private apiKey: string = "SQKAlq6Z1pKRaYGWJ4tK7kAvixqjeJn0"
+  private servicioURL: string = 'https://api.giphy.com/v1/gifs'
   private _historial: string[] = []
+
+
+  public resultados: Gif[] = []
 
   get historial() {
     return [...this._historial]
+  }
+
+  // inicializo el http para hacer peticiones con el modulo de angular
+  // se ejecuta sola una vez, se cargan los datos del local storage
+  constructor(private http: HttpClient) {
+
+    //obligo a que el item obtenido sea reconocido con !
+    this._historial = JSON.parse(localStorage.getItem("historial")!) || []
+    this.resultados = JSON.parse(localStorage.getItem("resultados")!) || []
   }
 
   buscarGifs(query: string) {
@@ -21,8 +37,22 @@ export class GifsService {
     if (!this._historial.includes(query)) {
       this._historial.unshift(query)
       this._historial = this._historial.splice(0, 10)
+
+      // guardo el historial en localstorage como un string
+      localStorage.setItem("historial", JSON.stringify(this._historial))
     }
 
-    console.log(this._historial)
+    const params = new HttpParams()
+      .set('apikey', this.apiKey)
+      .set('limit', '12')
+      .set('q', query);
+
+    // suscribe es como un then o await
+    this.http.get<SearchGifsResponse>(`${this.servicioURL}/search`, { params })
+      .subscribe((response) => {
+        // console.log(response.data)
+        this.resultados = response.data
+        localStorage.setItem("resultados", JSON.stringify(this.resultados))
+      })
   }
 }
